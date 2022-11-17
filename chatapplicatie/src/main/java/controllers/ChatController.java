e5b4d228d7615ab9af0e30b0216d63e5aae87b1b
Mitchell van Rijswijk <M.vanRijswijk2@student.han.nl>
2022-11-17 10:11:43 +0100

    Add Jakarta annotations in controller

--- chatapplicatie/src/main/java/controllers/ChatController.java
+++ chatapplicatie/src/main/java/controllers/ChatController.java
@@ -1,16 +1,27 @@
 package controllers;
 
 import data.dto.MessageDTO;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Application;
+import jakarta.ws.rs.core.MediaType;
 import models.Chat;
 
+@Path("/chats")
 public class ChatController {
+
     private Chat[] chats;
 
+    @GET
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
     public MessageDTO[] getChatHistory(Object sender, Object receiver){
         // code
         return null;
     }
 
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
     public void sendMessage(String message){
         // code
     }
