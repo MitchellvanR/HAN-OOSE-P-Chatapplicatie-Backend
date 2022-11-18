9c1484a0a52eb5bb45fbc14b50815cef9241c021
Mitchell van Rijswijk <M.vanRijswijk2@student.han.nl>
2022-11-18 16:04:28 +0100

    Fix message allocation

--- chatapplicatie/src/main/resources/front-end/request.js
+++ chatapplicatie/src/main/resources/front-end/request.js
@@ -45,7 +45,7 @@ const sendMessage = () => {
 function getChatLog(){
     sendHttpRequest('GET', 'http://localhost:8080/chatapplicatie/chats/1/2').then(responseData => {
       for (let message of responseData.messages){
-        if (message.senderId !== "1"){ //@todo het ID van de huidige gebruiker moet hier komen.
+        if (message.senderId === "1"){ //@todo het ID van de huidige gebruiker moet hier komen.
           this.outgoingMessage(message.content);
         } else {
           this.incomingMessage(message.content);
