801747a0d47dd364baf7907a42b7076ca6aa93ce
jderksen010 <jessevvdieren@gmail.com>
2022-11-28 13:56:07 +0100

    code clean up

--- chatapplicatie/src/main/java/jdi/chat/application/models/User.java
+++ chatapplicatie/src/main/java/jdi/chat/application/models/User.java
@@ -1,30 +1,15 @@
 package jdi.chat.application.models;
 
-import jdi.chat.application.network.LiveChatServer;
-
 public class User {
     private String id;
     private boolean online;
-    private LiveChatServer server = new LiveChatServer();
 
     public User(String id) {
         this.id = id;
     }
 
-    public void connect(){
-        // code
-    }
-
-    public void sendMessage(String message){
-        server.sendMessage(message);
-    }
-
     public String getId() {
         return id;
-    }
-
-    public boolean getOnline(){
-        return online;
     }
 
     public void setId(String id) {
