db5217e1bfb1a0bdc27d919881f4b8d39d564be7
Mitchell van Rijswijk <M.vanRijswijk2@student.han.nl>
2022-11-29 13:47:13 +0100

    Add test

---
+++ chatapplicatie/src/test/java/jdi/chat/application/util/ConfigTest.java
@@ -0,0 +1,14 @@
+package jdi.chat.application.util;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConfigTest {
+
+    @BeforeEach
+    public void setup() {
+
+    }
+
+}
