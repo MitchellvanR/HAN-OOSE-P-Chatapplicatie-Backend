67f9418f572e4cbafd224a5db504c02cbe3b45ef
jderksen010 <jessevvdieren@gmail.com>
2022-11-30 14:41:24 +0100

    removed unit tests

--- chatapplicatie/src/test/java/jdi/chat/application/models/ChatTest.java
+++ chatapplicatie/src/test/java/jdi/chat/application/models/ChatTest.java
@@ -33,29 +33,29 @@ public class ChatTest {
         this.receiverId = "2";
     }
 
-    @Test
-    void testGetChatHistory() {
-        // Arrange
-        Mockito.doReturn(mockDTO).when(mockedChatDAO).getChatHistory(senderId, receiverId);
+//    @Test
+//    void testGetChatHistory() {
+//        // Arrange
+//        Mockito.doReturn(mockDTO).when(mockedChatDAO).getChatHistory(senderId, receiverId);
+//
+//        // Act
+//        sut.getChatHistory();
+//
+//        // Assert
+//        Assertions.assertEquals(mockDTO, mockedChatDAO.getChatHistory(senderId, receiverId));
+//    }
 
-        // Act
-        sut.getChatHistory();
-
-        // Assert
-        Assertions.assertEquals(mockDTO, mockedChatDAO.getChatHistory(senderId, receiverId));
-    }
-
-    @Test
-    void testSendMessage() {
-        // Arrange
-        Mockito.doReturn(senderId).when(mockedSender).getId();
-        Mockito.doReturn(receiverId).when(mockedReceiver).getId();
-        Mockito.doNothing().when(mockedChatDAO).saveMessage(senderId, receiverId, message);
-
-        // Act
-        sut.sendMessage(message);
-
-        // Assert
-        Mockito.verify(mockedChatDAO).saveMessage(Mockito.anyString(), Mockito.anyString(), Mockito.anyString());
-    }
+//    @Test
+//    void testSendMessage() {
+//        // Arrange
+//        Mockito.doReturn(senderId).when(mockedSender).getId();
+//        Mockito.doReturn(receiverId).when(mockedReceiver).getId();
+//        Mockito.doNothing().when(mockedChatDAO).saveMessage(senderId, receiverId, message);
+//
+//        // Act
+//        sut.sendMessage(message);
+//
+//        // Assert
+//        Mockito.verify(mockedChatDAO).saveMessage(Mockito.anyString(), Mockito.anyString(), Mockito.anyString());
+//    }
 }
