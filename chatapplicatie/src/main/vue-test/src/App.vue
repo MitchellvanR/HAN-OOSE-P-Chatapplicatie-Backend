6e7cb8446b976d982ee674bf436f6ec2ebdcb716
jderksen010 <jessevvdieren@gmail.com>
2022-12-01 16:12:26 +0100

    add v1 prototype vue

---
+++ chatapplicatie/src/main/vue-test/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+  body {
+    background-color: #EEEEEE;
+    font-family: 'Montserrat', sans-serif;
+    display: grid;
+    grid-template-rows: auto;
+    justify-items: center;
+    padding-top: 50px;
+  }
+
+  body. html {
+    margin: 0;
+    height: 100%;
+  }
+
+  #app {
+    width: 50%;
+  }
+
+  nav {
+    padding: 20px 20px 20px 0;
+  }
+
+  nav a {
+    padding: 10px;
+    text-decoration: none;
+    background: #fff;
+    border-radius: 3px;
+    color: rgb(0, 110, 255);
+    font-weight: bold;
+    margin-right: 15px;
+  }
+</style>
