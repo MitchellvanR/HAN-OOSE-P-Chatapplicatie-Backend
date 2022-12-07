7ef7d828ae3e2195b20910c3e2de13ace9aff5b1
Thijs <TG.Vree@student.han.nl>
2022-12-07 14:45:35 +0100

    .

---
+++ chatapplicatie/src/test/java/jdi/chat/application/models/ChatTest.java
@@ -0,0 +1,19 @@
+//package jdi.chat.application.models;
+//
+//import jdi.chat.application.data.AbstractChatDAO;
+//import jdi.chat.application.data.SQLChatDAO;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.mockito.Mockito;
+//
+//class ChatTest {
+//    private Chat sut;
+//    private AbstractChatDAO mockedChatDAO;.
+//
+//    @BeforeEach
+//    void setup() {
+//        this.sut = new Chat("1");
+//        this.mockedChatDAO = Mockito.mock(SQLChatDAO.class);
+//
+//        sut.setChatDAO(mockedChatDAO);
+//    }
+//}
