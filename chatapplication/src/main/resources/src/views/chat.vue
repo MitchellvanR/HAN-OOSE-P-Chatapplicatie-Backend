74e69e0474d34ee87071b2570bc36c213cf64fd8
jderksen010 <jessevvdieren@gmail.com>
2023-01-10 15:10:21 +0100

    add 2 unit tests

--- chatapplication/src/main/resources/src/views/chat.vue
+++ chatapplication/src/main/resources/src/views/chat.vue
@@ -25,13 +25,13 @@
           <input type="text" id="message" placeholder="Write your message..." />
           <button class="btn" type="submit"><i class="fa fa-paper-plane-o" aria-hidden="true"></i></button>
           <button class="btn" type="button"><i class="fa fa-paperclip" aria-hidden="true"></i></button>
-          <button class="btn" type="button" @click="openForm()"><i class="fa fa-plus" aria-hidden="true"></i></button>
+          <button class="btn" type="button" id="openForm" @click="openForm()"><i class="fa fa-plus" aria-hidden="true"></i></button>
           <div class="form-popup" id="addUserToCurrentChat">
             <form id="addUserForm" class="wrap">
               <div class="row mb-2">
                 <input type="text" id="userId" placeholder="Enter userId" />
                 <button class="btn" type="submit"><i class="fa fa-check" aria-hidden="true"></i></button>
-                <button class="btn" type="button" @click="closeForm()"><i class="fa fa-times" aria-hidden="true"></i></button>
+                <button class="btn" type="button" id="closeForm" @click="closeForm()"><i class="fa fa-times" aria-hidden="true"></i></button>
               </div>
             </form>
           </div>
@@ -116,7 +116,6 @@ export default {
           this.cryptoKey,
           encodedMessage
       );
-
     },
     cleanForDecrypt: function (message) {
       let cleanMessage = message.replace(/['"]/g, '');
--- chatapplication/src/main/resources/tests/unit/chat.spec.js
+++ chatapplication/src/main/resources/tests/unit/chat.spec.js
@@ -1,41 +1,63 @@
 import {createLocalVue, mount, shallowMount} from '@vue/test-utils';
 import chat from '@/views/chat';
+import sinon from "sinon";
 
 const localVue = createLocalVue()
 
 describe('chat.vue', () => {
+    let wrapper;
+    const listener = sinon.spy();
     let getChatLog = jest.fn();
     let savePublicKey = jest.fn();
     let delay = jest.fn();
     let addUserToCurrentChat = jest.fn();
 
-    let wrapper = shallowMount(chat, {
-        methods: {
-            getChatLog,
-            savePublicKey,
-            delay,
-            addUserToCurrentChat
-        },
-        localVue
-    })
+    function setWrapper() {
+        wrapper = shallowMount(chat, {
+            methods: {
+                getChatLog,
+                savePublicKey,
+                delay,
+                addUserToCurrentChat
+            },
+            localVue
+        })
+    };
 
     it('is called OpenChat', () => {
+        setWrapper();
         expect(wrapper.name()).toEqual('OpenChat');
-    })
+    });
 
     it('calls getChatLog on mount', () => {
+        setWrapper();
         expect(getChatLog).toBeCalled();
-    })
+    });
 
     it('calls savePublicKey on mount', () => {
+        setWrapper();
         expect(savePublicKey).toBeCalled();
-    })
+    });
 
     it('calls delay on mount', () => {
+        setWrapper();
         expect(delay).toBeCalled();
-    })
+    });
 
     it('calls addUserToCurrentChat on mount', () => {
+        setWrapper();
         expect(addUserToCurrentChat).toBeCalled();
-    })
+    });
+
+    it('check if openForm is called after click button openForm', () => {
+        setWrapper();
+        wrapper.find('#openForm').trigger('click');
+        expect(listener.called);
+    });
+
+    it('check if closeForm is called after click button closeForm', () => {
+        setWrapper();
+        wrapper.find('#closeForm').trigger('click');
+        expect(listener.called);
+    });
 })
